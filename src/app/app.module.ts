874be9de84ee7276fea3ae08d3a2cd57044c881b
Jerika Kanz <jerikacapila@gmail.com>
2022-12-28 11:50:29 +0100

    Styling coding cube

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,20 @@ import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { ButtonModule } from 'primeng/button';
+import { InputTextModule } from 'primeng/inputtext';
+import { InputTextareaModule } from 'primeng/inputtextarea';
+import { CardModule } from 'primeng/card';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, FormsModule, ButtonModule],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    ButtonModule,
+    InputTextModule,
+    InputTextareaModule,
+    CardModule,
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
